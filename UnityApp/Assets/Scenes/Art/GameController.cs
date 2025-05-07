e51170c115804698bf36a7c015ba6416079cfaff
honglin.zhu <zucker>
2025-05-07 18:20:50 +0800

    fix: 支持点击模型交互；亭子旋转

--- UnityApp/Assets/Scenes/Art/GameController.cs
+++ UnityApp/Assets/Scenes/Art/GameController.cs
@@ -6,60 +6,53 @@ using System;
 
 public class GameController : MonoBehaviour
 {
-    public Button playButton;
+    public HandClicked handClicked;
     public AudioSource tingAudio;
     public TrackingManager tingTracking;
     public TrackingManager foxTracking;
     public TrackingManager screenTracking;
 
-    private Transform curTarget;
-    private Transform screenTarget;
+    public Transform tingTarget;
+    public Transform foxTarget;
+    public Transform screenTarget;
 
     // Start is called before the first frame update
     void Start()
     {
-        playButton.gameObject.SetActive(false);
-
-        playButton.onClick.RemoveAllListeners();
-        playButton.onClick.AddListener(()=> {
-            if (curTarget && curTarget.gameObject.activeSelf) {
-                playButton.gameObject.SetActive(false);
-                curTarget.GetComponent<Animator>().SetTrigger("Play");
-                if (curTarget.parent.name == "ImageTarget") 
-                {
-                    tingAudio.Play();
-                }
-                if (curTarget.parent.name == "ImageTarget1")
-                {
-                    screenTarget.GetComponent<Animator>().SetTrigger("Play");
-                }
+        handClicked.onClicked = (Transform trans) =>
+        {
+            if (trans.name == "AT") 
+            {
+                foxTarget.GetComponent<Animator>().SetTrigger("Play");
+                screenTarget.GetComponent<Animator>().SetTrigger("Play");
             }
-        });
+            if (trans.name == "Tingzi") {
+                tingTarget.GetComponent<Animator>().SetTrigger("Play");
+                tingAudio.Play();
+            }
+        };
 
         tingTracking.onTracked = (Transform trans) =>
         {
-            playButton.gameObject.SetActive(true);
-            curTarget = trans;
+
         };
         tingTracking.onLost = (Transform trans) =>
         {
             tingAudio.Stop();
-            playButton.gameObject.SetActive(false);
+            if (tingTarget) tingTarget.GetComponent<Animator>().Play("empty", -1, 0);
         };
 
         foxTracking.onTracked = (Transform trans) =>
         {
-            playButton.gameObject.SetActive(true);
-            curTarget = trans;
+
         };
         foxTracking.onLost = (Transform trans) =>
         {
-            playButton.gameObject.SetActive(false);
         };
 
         screenTracking.onTracked = (Transform trans) =>
         {
-            screenTarget = trans;
+  
         };
         screenTracking.onLost = (Transform trans) =>
         {
