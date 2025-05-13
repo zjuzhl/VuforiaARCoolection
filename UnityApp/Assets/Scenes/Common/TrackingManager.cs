003cfb8b557c0e14f4ba07ea9d8b13196a2f7969
honglin.zhu <zucker>
2025-05-13 18:19:04 +0800

    fix： 优化sample

--- UnityApp/Assets/Scenes/Common/TrackingManager.cs
+++ UnityApp/Assets/Scenes/Common/TrackingManager.cs
@@ -1,11 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class TrackingManager : MonoBehaviour
 {
-
     public Transform targetTrans;
+    public Action<Transform> onTracked;
+    public Action<Transform> onLost;
 
     // Start is called before the first frame update
     void Start()
@@ -13,19 +13,15 @@ public class TrackingManager : MonoBehaviour
         targetTrans.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void OnTargetTracked() 
     {
         targetTrans.gameObject.SetActive(true);
+        onTracked?.Invoke(targetTrans);
     }
 
     public void OnTargetLost()
     {
         targetTrans.gameObject.SetActive(false);
+        onLost?.Invoke(targetTrans);
     }
 }
