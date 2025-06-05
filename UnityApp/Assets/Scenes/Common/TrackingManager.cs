6737709f70012f39e6fab387bc9e2437f48fa638
honglin.zhu <zucker>
2025-06-05 10:56:13 +0800

    feat： 监听跟踪状态

--- UnityApp/Assets/Scenes/Common/TrackingManager.cs
+++ UnityApp/Assets/Scenes/Common/TrackingManager.cs
@@ -1,11 +1,23 @@
 using UnityEngine;
 using System;
+using Vuforia;
 
 public class TrackingManager : MonoBehaviour
 {
     public Transform targetTrans;
     public Action<Transform> onTracked;
     public Action<Transform> onLost;
+
+    public ObserverBehaviour mObserverBehaviour;
+    public bool trackingStatus 
+    {
+        get { return mObserverBehaviour.TargetStatus.Status == Status.TRACKED; }
+    }
+
+    public string trackingTargetName
+    {
+        get { return mObserverBehaviour.TargetName; }
+    }
 
     // Start is called before the first frame update
     void Start()
